f96edff13e3f71dda7324084fcacb30bb0494d9a
Ni7173 <nino.bravais20@gmail.com>
2024-03-08 17:41:47 +0100

    retrait min width sur mobile, chgmt effet sur skills_item et centrage texte dans projects-grid

--- js/global.js
+++ js/global.js
@@ -44,20 +44,11 @@ const observerIntersectionAnimation = () => {
 observerIntersectionAnimation();
 
 const observerSkillsAnimation = () => {
-    const skillLeft = document.getElementById('skill_item-2');
-    const skillsRight = document.querySelectorAll('.skill-item-right');
     const skills = document.querySelectorAll('.skills_item');
 
-    const setSkillLeft = () => {
-        skillLeft.style.opacity = "0";
-        skillLeft.style.transform = "translateX(220px)";
-        skillLeft.style.transition = "all 1.6s";
-    }
-    setSkillLeft();
-
-    skillsRight.forEach(skill => {
+    skills.forEach(skill => {
         skill.style.opacity = "0";
-        skill.style.transform = "translateX(-220px)";
+        skill.style.transform = "translateY(60px)";
         skill.style.transition = "all 1.6s";
     });
 
@@ -66,7 +57,7 @@ const observerSkillsAnimation = () => {
             if (entry.isIntersecting) {
                 let elem = entry.target
                 elem.style.opacity = "1";
-                elem.style.transform = "translateX(0px)";
+                elem.style.transform = "translateY(0px)";
             }
         });
     });
