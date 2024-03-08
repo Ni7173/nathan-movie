86bfc06dbfbaffe42e978940f9e40a9d87fa34ab
Ni7173 <nino.bravais20@gmail.com>
2024-03-08 09:45:54 +0100

    finitions plus qlq txt

--- js/global.js
+++ js/global.js
@@ -42,3 +42,38 @@ const observerIntersectionAnimation = () => {
 }
 
 observerIntersectionAnimation();
+
+const observerSkillsAnimation = () => {
+    const skillLeft = document.getElementById('skill_item-2');
+    const skillsRight = document.querySelectorAll('.skill-item-right');
+    const skills = document.querySelectorAll('.skills_item');
+
+    const setSkillLeft = () => {
+        skillLeft.style.opacity = "0";
+        skillLeft.style.transform = "translateX(220px)";
+        skillLeft.style.transition = "all 1.6s";
+    }
+    setSkillLeft();
+
+    skillsRight.forEach(skill => {
+        skill.style.opacity = "0";
+        skill.style.transform = "translateX(-220px)";
+        skill.style.transition = "all 1.6s";
+    });
+
+    let skillsObserver = new IntersectionObserver(function (entries, observer) {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                let elem = entry.target
+                elem.style.opacity = "1";
+                elem.style.transform = "translateX(0px)";
+            }
+        });
+    });
+
+    skills.forEach(skill => {
+        skillsObserver.observe(skill);
+    });
+}
+
+observerSkillsAnimation();
