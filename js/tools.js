5077d8a0a5d0da6c98d4fd37a28ed9c88e526de5
Ni7173 <nino.bravais20@gmail.com>
2024-03-09 15:14:27 +0100

    modifs suite au call: animations, traits à check...

---
+++ js/tools.js
@@ -0,0 +1,25 @@
+const observerElementsAnimation = () => {
+    const elements = document.querySelectorAll('.tool-part-container');
+
+    elements.forEach(element => {
+        element.style.opacity = "0";
+        element.style.transform = "translateY(60px)";
+        element.style.transition = "all 1.6s";
+    });
+
+    let elementsObserver = new IntersectionObserver(function (entries, observer) {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                let elem = entry.target
+                elem.style.opacity = "1";
+                elem.style.transform = "translateY(0px)";
+            }
+        });
+    });
+
+    elements.forEach(element => {
+        elementsObserver.observe(element);
+    });
+}
+
+observerElementsAnimation();
